ca0b5ab2ce74d51bbc2432b5d4656a67a7204e24
zizhao zhang <ssurmiczizhao@gmail.com>
2020-03-25 22:17:23 -0400

    first commit-start

---
+++ migrate_sub.py
@@ -0,0 +1,28 @@
+'''
+
+transfering tool for the subscriptions between yotube accounts
+
+reqiurements:
+
+module:
+
+standard library: re, xml.dom, time, collection
+extras: selenium
+
+
+'''
+
+
+
+from collections import namedtuple
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from xml.dom import minidom
+import time
+import re
+def main():
+    #create the instance of the driver
+    driver = webdriver.chrome()
+    sign_in_helper(driver)
+
+def sign_in_helper(driver):
